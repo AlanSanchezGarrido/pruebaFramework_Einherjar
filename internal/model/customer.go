fcb8dd3fc91320bd1f2a45bc3f21bda4f10defdf
AlanSanchezGarrido <dj_karli963@hotmail.com>
2026-08-17 22:21:20 -0600

    feat: agregar el modelo del cliente

--- internal/model/customer.go
+++ internal/model/customer.go
@@ -1,4 +1,15 @@
 package model
 
+import "time"
 
+type Customer struct {
+	ID        string
+	Name      string
+	LastName  string
+	CellPhone int
+	Email     string
+	Address    string
+	CreatedAt time.Timer
+	UpdatedAt time.Timer
+}
 
